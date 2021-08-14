fa93ca64a859022f81596db07da9e19ff0d9fd71
kerlw <kerl.d.w@gmail.com>
2021-08-14 22:25:17 +0800

    加入Ctrl+C退出程序的支持

--- src/main.rs
+++ src/main.rs
@@ -2,25 +2,33 @@ mod lexer;
 
 use crate::lexer::lexer::Lexer;
 use std::io::Write;
+use async_ctrlc::CtrlC;
+use async_std::prelude::*;
 
 const PROMPT: &str = ">>";
 
+#[async_std::main]
+async fn main() {
+    let ctrlc = CtrlC::new().expect("cannot create Ctrl+C handler!");
+    println!("Welcome to Monkey Language REPL, press Ctrl+C to quit.");
 
-fn main() {
-    loop {
-        print!("{}", PROMPT);
-        std::io::stdout().flush().unwrap();
-
-        let mut buf = String::new();
-        std::io::stdin().read_line(&mut buf).unwrap();
-
-        let mut lx = Lexer::new(buf);
+    ctrlc.race(async {
         loop {
-            let token = lx.next_token();
-            if token.is_eof() {
-                break;
+            print!("{}", PROMPT);
+            std::io::stdout().flush().unwrap();
+
+            let mut buf = String::new();
+            async_std::io::stdin().read_line(&mut buf).await.unwrap();
+
+            let mut lx = Lexer::new(buf);
+            loop {
+                let token = lx.next_token();
+                if token.is_eof() {
+                    break;
+                }
+                println!("{:?}", token);
             }
-            println!("{:?}", token);
         }
-    }
+    }).await;
+    println!("\nCtrl+C pressed, quiting")
 }
