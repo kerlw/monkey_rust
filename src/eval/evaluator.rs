6d25e66d0041d93250fb24d7f1f2994d7fc0aa0c
kerl.d.w <kerl.d.w@gmail.com>
2021-08-26 22:47:03 +0800

    ==和!=的eval

--- src/eval/evaluator.rs
+++ src/eval/evaluator.rs
@@ -30,6 +30,9 @@ fn eval_expression(expression: &Expression) -> Result<ObjectWrapper> {
             eval_infix_expression(left, operator, right)
         }
         Expression::PrefixExpression(operator, right) => eval_prefix_expression(operator, right),
+        Expression::IfExpression(condition, consequence, alternative) => {
+            eval_if_expression(condition, consequence, alternative)
+        }
         _ => Ok(ObjectWrapper::Null),
     }
 }
@@ -44,26 +47,41 @@ fn eval_infix_expression(
 
     match operator {
         Token::Plus => left.add(&right),
+        Token::Eq => left.eq(&right),
+        Token::NotEq => left.not_eq(&right),
         _ => Ok(ObjectWrapper::Null),
     }
 }
 
 fn eval_prefix_expression(operator: &Token, expr: &Expression) -> Result<ObjectWrapper> {
     match operator {
-        Token::Bang => {
-            match eval_expression(expr)? {
-                ObjectWrapper::Boolean(v) => Ok(ObjectWrapper::Boolean(!v)),
-                ObjectWrapper::Integer(v) => Ok(ObjectWrapper::Boolean(v == 0)),
-                _ => Ok(ObjectWrapper::Boolean(false)),
-            }
+        Token::Bang => match eval_expression(expr)? {
+            ObjectWrapper::Boolean(v) => Ok(ObjectWrapper::Boolean(!v)),
+            ObjectWrapper::Integer(v) => Ok(ObjectWrapper::Boolean(v == 0)),
+            _ => Ok(ObjectWrapper::Boolean(false)),
         },
-        Token::Minus => {
-            match eval_expression(expr)? {
-                ObjectWrapper::Integer(v) => Ok(ObjectWrapper::Integer(-v)),
-                ObjectWrapper::Float(f) => Ok(ObjectWrapper::Float(-f)),
-                _ => Err(format!("cannot eval {} after '-'.", operator.to_string()).into())
-            }
+        Token::Minus => match eval_expression(expr)? {
+            ObjectWrapper::Integer(v) => Ok(ObjectWrapper::Integer(-v)),
+            ObjectWrapper::Float(f) => Ok(ObjectWrapper::Float(-f)),
+            _ => Err(format!("cannot eval {} after '-'.", operator.to_string()).into()),
         },
         _ => Ok(ObjectWrapper::Null),
+    }
+}
+
+fn eval_if_expression(
+    condition: &Expression,
+    consequence: &Vec<Statement>,
+    alternative: &Vec<Statement>,
+) -> Result<ObjectWrapper> {
+    let cond = eval_expression(condition)?;
+    if let ObjectWrapper::Boolean(v) = cond {
+        if v {
+            eval_statements(consequence)
+        } else {
+            eval_statements(alternative)
+        }
+    } else {
+        return Err("Invalid 'if' condition.".into());
     }
 }
--- src/eval/mod.rs
+++ src/eval/mod.rs
@@ -13,6 +13,18 @@ pub enum ObjectWrapper {
     Boolean(bool),
 }
 
+fn ensure_compare_with_same_type(one: &ObjectWrapper, two: &ObjectWrapper) -> Result<()> {
+    if !one.type_str().eq(two.type_str()) {
+        return Err(format!(
+            "Cannot compare between different types:{} and {}",
+            one.type_str(),
+            two.type_str()
+        )
+        .into());
+    }
+    Ok(())
+}
+
 impl ObjectWrapper {
     pub fn type_str(&self) -> &str {
         match self {
@@ -32,6 +44,46 @@ impl ObjectWrapper {
                 _ => Err(format!("int cannot '+' with type {}.", other.type_str()).into()),
             },
             _ => Err(format!("type {} dose not support '+' operation.", self.type_str()).into()),
+        }
+    }
+
+    pub fn eq(&self, other: &Self) -> Result<Self> {
+        ensure_compare_with_same_type(self, other)?;
+
+        match (self, other) {
+            (ObjectWrapper::Integer(one), ObjectWrapper::Integer(two)) => {
+                Ok(ObjectWrapper::Boolean(one == two))
+            }
+            (ObjectWrapper::Float(one), ObjectWrapper::Float(two)) => {
+                Ok(ObjectWrapper::Boolean(one == two))
+            }
+            (ObjectWrapper::Boolean(one), ObjectWrapper::Boolean(two)) => {
+                Ok(ObjectWrapper::Boolean(one == two))
+            }
+            (ObjectWrapper::Null, ObjectWrapper::Null) => {
+                Ok(ObjectWrapper::Boolean(true))
+            }
+            _ => Ok(ObjectWrapper::Boolean(false)),
+        }
+    }
+
+    pub fn not_eq(&self, other: &Self) -> Result<Self> {
+        ensure_compare_with_same_type(self, other)?;
+
+        match (self, other) {
+            (ObjectWrapper::Integer(one), ObjectWrapper::Integer(two)) => {
+                Ok(ObjectWrapper::Boolean(one != two))
+            }
+            (ObjectWrapper::Float(one), ObjectWrapper::Float(two)) => {
+                Ok(ObjectWrapper::Boolean(one != two))
+            }
+            (ObjectWrapper::Boolean(one), ObjectWrapper::Boolean(two)) => {
+                Ok(ObjectWrapper::Boolean(one != two))
+            }
+            (ObjectWrapper::Null, ObjectWrapper::Null) => {
+                Ok(ObjectWrapper::Boolean(false))
+            }
+            _ => Ok(ObjectWrapper::Boolean(true)),
         }
     }
 }
